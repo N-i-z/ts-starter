c3abe4eb78f70914e6f70fb9446a595ecd2a4ecd
Nisma Nizam <fnismanizam@gmail.com>
2024-08-30 22:13:07 +0530

    build: Setup API 4th session

--- src/posts/posts.service.ts
+++ src/posts/posts.service.ts
@@ -1,49 +1,50 @@
-import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
-import CreatePostDto from './dto/createPost.dto';
-import Post from './post.interface';
-import UpdatePostDto from './dto/updatePost.dto';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreatePostDto } from './dto/createPost.dto';
+import { UpdatePostDto } from './dto/updatePost.dto';
+import Post from './post.entity'; // Assume you have an entity class Post
 
 @Injectable()
-export default class PostsService {
-  private lastPostId = 0;
-  private posts: Post[] = [];
+export class PostsService {
+  private posts: Post[] = []; // Simulated data storage for posts
 
   getAllPosts() {
     return this.posts;
   }
 
-  getPostById(id: number) {
+  getPostById(id: number): Post {
     const post = this.posts.find((post) => post.id === id);
-    if (post) {
-      return post;
+    if (!post) {
+      throw new NotFoundException('Post not found');
     }
-    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    return post;
   }
 
-  replacePost(id: number, post: UpdatePostDto) {
-    const postIndex = this.posts.findIndex((post) => post.id === id);
-    if (postIndex > -1) {
-      this.posts[postIndex] = post;
-      return post;
-    }
-    throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
-  }
-
-  createPost(post: CreatePostDto) {
-    const newPost = {
-      id: ++this.lastPostId,
-      ...post,
-    };
+  createPost(postData: CreatePostDto) {
+    const newPost = { id: Date.now(), ...postData }; // Simulated ID generation
     this.posts.push(newPost);
     return newPost;
   }
 
+  replacePost(id: number, postData: UpdatePostDto) {
+    const postIndex = this.posts.findIndex((post) => post.id === id);
+    if (postIndex === -1) {
+      throw new NotFoundException('Post not found');
+    }
+    this.posts[postIndex] = { id, ...postData };
+    return this.posts[postIndex];
+  }
+
+  updatePost(id: number, postData: UpdatePostDto) {
+    const post = this.getPostById(id);
+    Object.assign(post, postData); // Merge the partial update
+    return post;
+  }
+
   deletePost(id: number) {
     const postIndex = this.posts.findIndex((post) => post.id === id);
-    if (postIndex > -1) {
-      this.posts.splice(postIndex, 1);
-    } else {
-      throw new HttpException('Post not found', HttpStatus.NOT_FOUND);
+    if (postIndex === -1) {
+      throw new NotFoundException('Post not found');
     }
+    this.posts.splice(postIndex, 1);
   }
 }
